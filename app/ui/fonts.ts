15f1acf6bbd67c8c67fff09763729ff9c1857cbc
seattleriot <seattleriot@hotmail.com>
2025-07-24 14:07:38 -0700

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Catamaran, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const catamaran = Catamaran({ weight: '900' });
+export const lusitana = Lusitana({ weight: '400' });
