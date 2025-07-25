db5e5ed2921f2dd392def6315f9711009c282b3b
seattleriot <seattleriot@hotmail.com>
2025-07-25 10:44:39 -0700

    Adding subsets to google font imports

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
 import { Inter, Catamaran, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const catamaran = Catamaran({ weight: '900' });
-export const lusitana = Lusitana({ weight: '400' });
+export const catamaran = Catamaran({ subsets: ['latin'], weight: '900' });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
